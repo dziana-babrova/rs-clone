03ec1e9a9bb501b133ad732452f3b9c2d816ff51
Diana_Bobrova <dziana_babrova@epam.com>
2023-02-07 23:49:53 +0300

    fix: physics for tiles

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import Phaser from 'phaser';
+import Phaser, { Game, Types, AUTO } from 'phaser';
 import GameScene from 'scenes/game-scene/GameScene';
-=======
 import gameConfig from 'gameConfig';
-import { Game, Types, AUTO } from 'phaser';
->>>>>>> f1064bd0c8b7ac459abc0127d6b8fc09150c6362
 import PreloadScene from 'scenes/preload-scene/PreloadScene';
-import GameScene from './scenes/game-scene/GameScene';
 
 const config: Types.Core.GameConfig = {
   type: AUTO,
@@ -28,10 +23,6 @@ const config: Types.Core.GameConfig = {
       gravity: true,
     },
   },
-<<<<<<< HEAD
-  scene: [PreloadScene, GameScene],
-=======
->>>>>>> f1064bd0c8b7ac459abc0127d6b8fc09150c6362
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/scenes/game-scene/components/Platform.ts
+++ src/scenes/game-scene/components/Platform.ts
@@ -1,10 +1,10 @@
-export default class TilesSingle extends Phaser.Physics.Arcade.Sprite {
+export default class TilesSingle extends Phaser.Physics.Matter.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y, texture);
+    super(scene.matter.world, x, y, texture);
 
     this.setOrigin(0, 0);
     this.setTexture('platforms', texture);
+    this.setStatic(true);
     scene.add.existing(this);
-    scene.physics.add.existing(this, true);
   }
 }
