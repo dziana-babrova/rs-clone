b2de6d57be06934b7f048dfd3a7cef326c3ae9c9
Alex Ch <sashkill94@gmail.com>
2023-02-08 19:11:55 +0300

    feat: add basic server and routing to /api/auth

---
+++ server/errors/ApiError.ts
@@ -0,0 +1,22 @@
+import { ValidationError } from 'express-validator';
+
+export class ApiError extends Error {
+  status: number;
+  errors: ValidationError[] | unknown[];
+
+  constructor(status: number, message: string, errors: ValidationError[] | unknown[] = []) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+
+  static UnauthorizedError() {
+    return new ApiError(401, 'Unauthorized');
+  }
+  static BadRequest(message: string, errors: ValidationError[] | unknown[] = []) {
+    return new ApiError(400, message, errors);
+  }
+  static NotFound(message: string) {
+    return new ApiError(404, message);
+  }
+}
